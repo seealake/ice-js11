8396dc2061a495fa217658f30424e6e5f19fe07e
Daniel Smedema <7870631+djsmedes@users.noreply.github.com>
2025-11-10 14:41:10 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const NAMES = ["Jessie", "Jackson", "Ben", "Kyle", "Gabby", "Jill"];
+const NUMS = [2, 5, 6, 8, 19, 22, 27];
+
+/**
+ *  Add event listeners such that the buttons with the following ids
+ *  perform the corresponding calculation on the lists of NAMES and NUMS.
+ * 
+ * `btn-log-names`       Prints out each name on a new line
+ * `btn-first-initial`   Prints out the list of each person's first initial
+ * `btn-length-of-name`  Prints out the list of each person's name length
+ * `btn-j-names`         Prints out the list of people who's name starts with J
+ * `btn-long-j-names`    Prints out the list of people who's name starts with J and have a long name (>= 5 characters)
+ * `btn-is-there-7`      Prints out whether or not there is a 7 in the list of numbers
+ * `btn-is-all-positive` Prints out whether or not each number is positive
+ */
