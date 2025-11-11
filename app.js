5769e1fe3fd6c2932f2d2acf9eeb69195bf5631e
Yulong Wu <wu.yulong@wustl.edu>
2025-11-10 18:02:25 -0600

    update

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const NUMS = [2, 5, 6, 8, 19, 22, 27];
  *
  *  You can choose `keypress`, `click`, `focus`, or anything else you want to try!
  */
-
+const inputElement = document.getElementById("input");
+inputElement.addEventListener("focus", () => {
+  console.log("Input element focused");
+});
 /**
  * PART 2:
  *  Add event listeners such that the buttons with the following ids
@@ -22,6 +25,34 @@ const NUMS = [2, 5, 6, 8, 19, 22, 27];
  * `btn-is-there-7`      Call writeOutput with whether or not there is a 7 in the list of numbers
  * `btn-is-all-positive` Call writeOutput with whether or not each number is positive
  */
+document.getElementById("btn-log-names").addEventListener("click", () => {
+  NAMES.forEach(name => console.log(name));
+}
+);
+document.getElementById("btn-first-initial").addEventListener("click", () => {
+  const initials = NAMES.map(name => name.charAt(0));
+  writeOutput(initials);
+});
+document.getElementById("btn-length-of-name").addEventListener("click", () => {
+  const nameLengths = NAMES.map(name => name.length);
+  writeOutput(nameLengths);
+});
+document.getElementById("btn-j-names").addEventListener("click", () => {
+  const jNames = NAMES.filter(name => name.startsWith("J"));
+  writeOutput(jNames);
+}); 
+document.getElementById("btn-long-j-names").addEventListener("click", () => {
+  const longJNames = NAMES.filter(name => name.startsWith("J") && name.length >= 5);
+  writeOutput(longJNames);
+});
+document.getElementById("btn-is-there-7").addEventListener("click", () => {
+  const hasSeven = NUMS.includes(7);
+  writeOutput(hasSeven);
+}); 
+document.getElementById("btn-is-all-positive").addEventListener("click", () => {
+  const allPositive = NUMS.every(num => num > 0);
+  writeOutput(allPositive);
+});
 
 function writeOutput(stringOrArray) {
   const outputNode = document.getElementById("output");
